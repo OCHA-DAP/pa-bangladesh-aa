d2c38b9def6440f2a9c742e770d05deb1b817a14
hannahker <hannah.ker96@gmail.com>
2020-10-26 12:13:41 +0000

    Restructure folders

---
+++ scripts/d02_processing/flood_extent.py
@@ -0,0 +1,49 @@
+import geopandas as gpd
+import pandas as pd
+import datetime
+import os
+
+DATA_DIR = 'data'
+OUTPUT_DIR = 'data'
+SHP_DIR = 'Shapefiles'
+
+
+def calc_extent():
+    """
+    Calculate the extent of flooding for ADM4 regions using the shapefiles output from GEE script.
+    dirname = name of folder with the shapefiles output from
+    """
+
+    # Read in the shapefile for Bangladesh
+    unions_shp = gpd.read_file(os.path.join(SHP_DIR, 'bdg_shp/bgd_admbnda_adm4_bbs_20180410.shp'))
+    # Select unions of interest
+    unions_shp = unions_shp[unions_shp['ADM2_EN'].isin(['Bogra', 'Gaibandha', 'Jamalpur', 'Kurigram', 'Sirajganj'])]
+    # Set the crs
+    unions_shp = unions_shp.to_crs('ESRI:54009')
+    # Get geom for the unions
+    unions_shp.loc[:, 'union_area'] = unions_shp['geometry'].area
+    # Get all of the dates from the shapefiles in the directory
+    dates = []
+    for d in os.listdir(SHP_DIR):
+        sp = d.split('-')
+        if d.startswith('BGD') and d.endswith('shp'):
+            dates.append((sp[1] + '-' + sp[2] + '-' + sp[3] + '-' + sp[4] + '-' + sp[5]).split('.')[0])
+    # Create the output dataframe
+    output_df = pd.DataFrame()
+    # Loop through all shapefiles and calculate the flood extent
+    for date in dates:
+        print(date)
+        fname = os.path.join(SHP_DIR + f'/BGD_Floods-{date}.shp')
+        print(fname)
+        flood_shp = gpd.read_file(fname)
+        flood_shp = flood_shp.to_crs('ESRI:54009')
+        intersection = gpd.overlay(unions_shp, flood_shp, how='intersection')
+        intersection = intersection.dissolve(by='ADM4_PCODE')
+        flood_extent = intersection['geometry'].area
+        flood_extent = flood_extent.rename('flooded_area')
+        output_df_part = pd.merge(unions_shp, flood_extent.to_frame(), left_on='ADM4_PCODE', right_index=True)
+        output_df_part.loc[:, 'date'] = datetime.datetime.strptime(date[:10], '%Y-%m-%d')
+        output_df = output_df.append(output_df_part)
+    output_df.loc[:, 'flood_fraction'] = output_df['flooded_area'] / output_df['union_area']
+    output_df.to_excel(os.path.join(DATA_DIR, 'Sentinel-1-BGD-Flooding-TOTAL.xlsx'))
+    return output_df

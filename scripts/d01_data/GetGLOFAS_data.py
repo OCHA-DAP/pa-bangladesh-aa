03c4d96a39dcfadbf18cfafdf84ae08740851e5b
Leonardo Milano <milanoleonardo@yahoo.it>
2020-11-04 16:36:28 +0100

    updating script to get historical data from all the FFWC stations along the Jamuna river basin

--- scripts/d01_data/GetGLOFAS_data.py
+++ scripts/d01_data/GetGLOFAS_data.py
@@ -9,11 +9,24 @@ import numpy as np
 from datetime import date, timedelta
 import shutil
 
+# Download raster data from GLOFAS and extracts time series of water discharge in selected locations, matching the FFWC stations 
+# data from https://cds.climate.copernicus.eu/cdsapp#!/dataset/cems-glofas-historical?tab=overview
 
-Bahadurabat_GLOFAS_LON_LAT=[89.65,25.15]
+# location of stations on the Jamuna/Brahmaputra river from http://www.ffwc.gov.bd/index.php/googlemap?id=20
+# some lat lon indicated by FFWC are not on the river and have been manually moved to the closest pixel on the river
+FFWC_Stations_lonlat={
+    'Noonkhawa':[89.9509,25.9496],
+    'Chilmari':[89.7476,25.5451],
+    'Bahadurabad':[89.6607,25.1028],
+    'Sariakandi':[89.6518,24.8901],
+    'Kazipur':[89.7498,24.6637],
+    'Serajganj':[89.7479,24.4676],
+    'Aricha':[89.6550,23.9032]
+}
+
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 GLOFAS_DS_FILENAME='CEMS_ECMWF_dis24_{}_glofas_v2.1.nc'
-GLOFAS_DS_FOLDER='GLOFAS_data'
+GLOFAS_DS_FOLDER='../../data/raw/GLOFAS_data'
 
 def unzip(zip_file_path, save_path):
     print(f'Unzipping {zip_file_path}')
@@ -54,7 +67,7 @@ def get_GLOFAS_zip(c_api,year,folder):
     },
     '{}/download_{}.zip'.format(folder,year))
 
-def extract_dis24_val(date,folder):
+def extract_dis24_values(date,folder,glofas_df):
     try:
         glofas_ds_name='{}/{}'.format(folder,GLOFAS_DS_FILENAME.format(date.strftime("%Y%m%d")))
         glofas = Dataset(glofas_ds_name,'r')
@@ -62,34 +75,37 @@ def extract_dis24_val(date,folder):
         lats = glofas.variables['lat'][:] 
         lons = glofas.variables['lon'][:]
         
-        # latitude lower and upper index
-        lonbound = np.argmin(np.abs( lons - Bahadurabat_GLOFAS_LON_LAT[0]))
-        latbound = np.argmin(np.abs( lats - Bahadurabat_GLOFAS_LON_LAT[1]))
-
-        # variables are lon,lat,time,dis24
-        discharge = glofas.variables['dis24'][:,latbound,lonbound]
-        return discharge
-    except Exception:
-        # print(f'discharge data not available for {date}')
-        return None
+        # loop over stations
+        for station_name, station_lonlat in FFWC_Stations_lonlat.items():
+            # latitude lower and upper index
+            lonbound = np.argmin(np.abs( lons - station_lonlat[0]))
+            latbound = np.argmin(np.abs( lats - station_lonlat[1]))
+            # variables are lon,lat,time,dis24
+            discharge = glofas.variables['dis24'][:,latbound,lonbound]
+            if discharge is not None:
+                glofas_df.at[date,f'dis24_{station_name}']=discharge
+        return glofas_df
+    except Exception as e:
+        print(f'discharge data not available for {date}')
+        print(e)
+        return glofas_df
 
 def get_glofas_df(year,folder):
     start_date = date(year, 1, 1)
-    end_date = date(year, 12, 31)
+    end_date = date(year, 1, 2)
+    # end_date = date(year, 12, 31)
     delta = timedelta(days=1)
-    glofas_df=pd.DataFrame(columns=['dis24'])
+    glofas_df=pd.DataFrame()
     while start_date <= end_date:
         # print(start_date.strftime("%Y-%m-%d"))
-        glofas_discharge=extract_dis24_val(start_date,folder)
-        if glofas_discharge is not None:
-            glofas_df.at[start_date,'dis24']=glofas_discharge
+        glofas_df=extract_dis24_values(start_date,folder,glofas_df)
         start_date += delta
     glofas_df.index=pd.to_datetime(glofas_df.index)
+    glofas_df.index.name = 'date'
     glofas_df = glofas_df.resample('D').mean().interpolate(method='linear')
     return glofas_df
 
 c = cdsapi.Client()
-year=2019
 
 for year in range(1979,2021):
     print(year)

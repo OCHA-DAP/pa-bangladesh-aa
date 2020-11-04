3a3baae698c7e124c821d3c146e6f6e03389e8fd
hannahker <hannah.ker96@gmail.com>
2020-11-04 10:26:17 +0000

    Dissolve polygons if necessary

--- scripts/d02_processing/FE_flood_extent.py
+++ scripts/d02_processing/FE_flood_extent.py
@@ -6,20 +6,22 @@ import os
 DATA_DIR = 'data/processed'
 SHP_DIR = 'Shapefiles'
 
-def calc_extent(admin):
+def calc_extent(ADM_GRP):
     """
     Calculate the extent of flooding for ADM4 regions using the shapefiles output from GEE script.
     dirname = name of folder with the shapefiles output from
     """
 
     # Read in the shapefile for Bangladesh
-    unions_shp = gpd.read_file(os.path.join(DATA_DIR, SHP_DIR, 'bdg_shp/bgd_admbnda_adm4_bbs_20180410.shp'))
+    adm_shp = gpd.read_file(os.path.join(DATA_DIR, SHP_DIR, 'bdg_shp/bgd_admbnda_adm4_bbs_20180410.shp'))
     # Select unions of interest
-    unions_shp = unions_shp[unions_shp['ADM2_EN'].isin(['Bogra', 'Gaibandha', 'Jamalpur', 'Kurigram', 'Sirajganj'])]
+    adm_shp = adm_shp[adm_shp['ADM2_EN'].isin(['Bogra', 'Gaibandha', 'Jamalpur', 'Kurigram', 'Sirajganj'])]
     # Set the crs
-    unions_shp = unions_shp.to_crs('ESRI:54009')
-    # Get geom for the unions
-    unions_shp.loc[:, 'union_area'] = unions_shp['geometry'].area
+    adm_shp = adm_shp.to_crs('ESRI:54009')
+    # Dissolve the shp if necessary
+    if ADM_GRP != 'ADM4_EN':
+        adm_shp = adm_shp.dissolve(by=ADM_GRP).reset_index()
+    adm_shp.loc[:, 'adm_area'] = adm_shp['geometry'].area
     # Get all of the dates from the shapefiles in the directory
     dates = []
     for d in os.listdir(os.path.join(DATA_DIR, SHP_DIR)):
@@ -34,15 +36,15 @@ def calc_extent(admin):
         fname = os.path.join(DATA_DIR, SHP_DIR + f'/BGD_Floods-{date}.shp')
         flood_shp = gpd.read_file(fname)
         flood_shp = flood_shp.to_crs('ESRI:54009')
-        intersection = gpd.overlay(unions_shp, flood_shp, how='intersection')
-        intersection = intersection.dissolve(by=admin)
+        intersection = gpd.overlay(adm_shp, flood_shp, how='intersection')
+        intersection = intersection.dissolve(by=ADM_GRP)
         flood_extent = intersection['geometry'].area
         flood_extent = flood_extent.rename('flooded_area')
-        output_df_part = pd.merge(unions_shp, flood_extent.to_frame(), left_on=admin, right_index=True)
+        output_df_part = pd.merge(adm_shp, flood_extent.to_frame(), left_on=ADM_GRP, right_index=True)
         output_df_part.loc[:, 'date'] = datetime.datetime.strptime(date[:10], '%Y-%m-%d')
         output_df = output_df.append(output_df_part)
-    output_df.loc[:, 'flood_fraction'] = output_df['flooded_area'] / output_df['union_area']
-    output_df.to_excel(os.path.join(DATA_DIR, f'Sentinel-1-BGD-Flooding-{admin}-TOTAL.xlsx'))
+    output_df.loc[:, 'flood_fraction'] = output_df['flooded_area'] / output_df['adm_area']
+    output_df.to_excel(os.path.join(DATA_DIR, f'Sentinel-1-BGD-Flooding-{ADM_GRP}-TOTAL.xlsx'))
     return output_df
 
 
